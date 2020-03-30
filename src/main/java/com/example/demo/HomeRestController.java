be24430c26041eb8c94417604bbc66bc3cdd4c1c
reddappa-cyber <tech.reddy786@gmail.com>
2020-03-30 12:58:34 +0530

    changed HomeRestController

--- src/main/java/com/example/demo/HomeRestController.java
+++ src/main/java/com/example/demo/HomeRestController.java
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-
-package com.example.demo;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class HomeRestController {
-
-	@GetMapping("/show")
-	public String showMessage() {
-		return "WLCOME TO SPRING BOOT";
-	}
-}
-=======
 /*
  * package com.example.demo;
  * 
@@ -25,4 +9,3 @@ public class HomeRestController {
  * @GetMapping("/show") public String showMessage() { return
  * "WLCOME TO SPRING BOOT"; } }
  */
->>>>>>> development
