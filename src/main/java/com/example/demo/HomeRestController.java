359e2e41f2f138a93bce59fda9996fe43f583fea
reddappa-cyber <tech.reddy786@gmail.com>
2020-03-30 12:15:38 +0530

    adding code.txt file

--- src/main/java/com/example/demo/HomeRestController.java
+++ src/main/java/com/example/demo/HomeRestController.java
@@ -1,13 +1,11 @@
-package com.example.demo;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class HomeRestController {
-
-	@GetMapping("/show")
-	public String showMessage() {
-		return "WLCOME TO SPRING BOOT";
-	}
-}
+/*
+ * package com.example.demo;
+ * 
+ * import org.springframework.web.bind.annotation.GetMapping; import
+ * org.springframework.web.bind.annotation.RestController;
+ * 
+ * @RestController public class HomeRestController {
+ * 
+ * @GetMapping("/show") public String showMessage() { return
+ * "WLCOME TO SPRING BOOT"; } }
+ */
